8a4ea63efd1f01abc7b5f25588ab0347a095705b
DewmiKarunarathna <indutharak04@gmail.com>
2025-12-23 22:18:15 +0530

    Add class definition for codeBuddy chatbot

--- codeBuddy.py
+++ codeBuddy.py
@@ -4,3 +4,7 @@ import datetime #get current date, time, formatting
 import random #used to make random choices
 import re #smart text pattern recognition
 from typing import Dict, List, Optional #use hints for better communication - returns a list, returns a dictionary,...
+
+class codeBuddy:
+    """ An advanced AI chatbot for programming assistance"""
+    
